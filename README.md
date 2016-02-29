f529108a0fbe73aafae9b9e9f11980a392af96bf
Bill Jackson <bill@brainclippings.com>
2016-02-29 10:58:35 -0500

    Initial project commit

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# cleaning-data-project
-Coursera Getting and Cleaning Data Project Assignment
+# Getting and Cleaning Data - Course Project
+
+This is the course project for the Getting and Cleaning Data Coursera course.
+The R script, `run_analysis.R`, does the following:
+
+1. Loads the data sets from the working directory / data
+2. Load the activity and feature info
+3. Loads both the training and test datasets, keeping only mean or standard deviation
+4. Loads and merges subject and activity data
+5. Merges the two sets of data
+6. Converts the `activity` and `subject` columns into factors
+7. Creates a tidy dataset that consists of the mean() value of each
+   variable for each subject and activity grouping
+
+The end result is saved in the file `tidy_means.txt`.
---
+++ run_analysis.R
@@ -0,0 +1,43 @@
+library(reshape2)
+
+filename <- "getdata_dataset.zip"
+
+# activity and features and cast
+activityLabels <- read.table("data/activity_labels.txt")
+activityLabels[, 2] <- as.character(activityLabels[, 2])
+features <- read.table("data/features.txt")
+features[, 2] <- as.character(features[, 2])
+
+# only want the mean and sd stuff
+desired_features <- grep(".*mean.*|.*std.*", features[,2])
+desired_features.names <- features[desired_features,2]
+desired_features.names = gsub('-mean', 'Mean', desired_features.names)
+desired_features.names = gsub('-std', 'Std', desired_features.names)
+desired_features.names <- gsub('[-()]', '', desired_features.names)
+
+# actual training data
+train <- read.table("data/train/X_train.txt")[desired_features]
+trainAct <- read.table("data/train/Y_train.txt")
+trainSubj <- read.table("data/train/subject_train.txt")
+train <- cbind(trainSubj, trainAct, train)
+
+# actual test data
+test <- read.table("data/test/X_test.txt")[desired_features]
+testAct <- read.table("data/test/Y_test.txt")
+testSubj <- read.table("data/test/subject_test.txt")
+test <- cbind(testSubj, testAct, test)
+
+# merge and name
+allData <- rbind(train, test)
+colnames(allData) <- c("subject", "activity", desired_features.names)
+
+# factorize
+allData$activity <- factor(allData$activity, levels = activityLabels[,1], labels = activityLabels[,2])
+allData$subject <- as.factor(allData$subject)
+
+# melt it to change the shape
+allData.melted <- melt(allData, id = c("subject", "activity"))
+
+#write out just the mean to a tidy file
+allData.mean <- dcast(allData.melted, subject + activity ~ variable, mean)
+write.table(allData.mean, "tidy_means.txt", row.names = FALSE, quote = FALSE)
